07d3a9e538cb4ff1f2bfcace69f9a490950eb1a9
Riaan Attar <attarriaan6226@gmail.com>
2026-04-19 01:31:13 +0530

    dev ops 1

--- frontend/components/ui/SmoothScroll.tsx
+++ frontend/components/ui/SmoothScroll.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import Lenis from '@studio-freight/lenis';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
